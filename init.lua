c0ba5b0b339a8a5a13414e370b3dc2d74310a735
Norman Rosner <norman@rosner.io>
2015-03-10 00:17:06 +0100

    Add simple splitting and maximizing for focused window.

--- init.lua
+++ init.lua
@@ -9,8 +9,8 @@ local hipchat_id = 'com.hipchat.HipChat'
 local messages_id = 'com.apple.iChat'
 
 -- shortcuts
-local shiftctrl = {"shift", "ctrl"}
-local ctrlaltcmd = {"ctrl", "alt", "cmd"}
+local shiftctrl = {'shift', 'ctrl'}
+local ctrlaltcmd = {'ctrl', 'alt', 'cmd'}
 
 function focus_and_maximize(app_id)
     local app = application.applicationsforbundleid(app_id)[1]
@@ -20,7 +20,33 @@ function focus_and_maximize(app_id)
 end
 
 -- Focus hipchat and maximize its window on the current screen
-hotkey.bind(ctrlaltcmd, "H", function()
+hotkey.bind(ctrlaltcmd, 'H', function()
     focus_and_maximize(hipchat_id)
+end
+)
+
+-- vim like splits
+hotkey.bind(shiftctrl, 'H', function()
+    window.focusedwindow():movetounit({x=0, y=0, w=0.5, h=1.0})
+end
+)
+
+hotkey.bind(shiftctrl, 'L', function()
+    window.focusedwindow():movetounit({x=0.5, y=0, w=0.5, h=1})
+end
+)
+
+hotkey.bind(shiftctrl, 'J', function()
+    window.focusedwindow():movetounit({x=0, y=0.5, w=1, h=0.5})
+end
+)
+
+hotkey.bind(shiftctrl, 'K', function()
+    window.focusedwindow():movetounit({x=0, y=0, w=1, h=0.5})
+end
+)
+
+hotkey.bind(shiftctrl, 'M', function()
+    window.focusedwindow():maximize()
 end
 )
